192d6aa23ff3aad9e7d02e9b521d313dd168243a
iemprashant <prashantgupta9876@gmail.com>
2020-06-21 14:59:11 +0530

    adding movement

--- snake.js
+++ snake.js
@@ -5,40 +5,53 @@ function init(){
 	pen = canvas.getContext('2d');
 	cellsize = 66;
 	score = 5;
-
-
-
 	snake = {
 		init_len:5,
 		color:"blue",
 		cells:[],
 		direction:"right",
 
-
 		createSnake:function(){
 			for(var i=this.init_len;i>0;i--){
 				this.cells.push({x:i,y:0});
 			}
 		},
+
 		drawSnake:function(){
 
 			for(var i=0;i<this.cells.length;i++){
+				pen.fillStyle=this.color;
 				pen.fillRect(this.cells[i].x*cellsize,this.cells[i].y*cellsize,cellsize-3,cellsize-3);
 			}
-        }
+		},
+		updateSnake:function(){
+			this.cells.pop();
+			var headX =this.cells[0].x;
+			var headY =this.cells[0].y;
+			var X =headX+1;
+			var Y=headY;
+			this.cells.unshift({x:X,y:Y});
+			
+		}
 
 	};
 
 	snake.createSnake();
 }
-// function update(){
-
-// };
-function gameloop(){
+function draw(){
+	pen.clearRect(0,0,W,H);
     snake.drawSnake();
-    update;
+}
+function update(){
+	snake.updateSnake();
+}
+function gameloop(){
+	draw();
+    update();
 }; 
+
 init();
+
 var f = setInterval(gameloop,1000);
 
 
